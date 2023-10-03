1127b60f55dd13cba9d8203baaaef8954ba8c23d
Nathan <71136851+arandrollcode@users.noreply.github.com>
2023-10-02 23:16:44 -0500

    Synced everything to Flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template('ecoEats.html')
+
+@app.route("/test")
+def test():
+    return render_template('test.html')
