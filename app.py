7f4286fd5e1301ec17a108e2b18fd419812df693
Nathan <71136851+arandrollcode@users.noreply.github.com>
2023-10-02 23:20:41 -0500

    commited flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +9,4 @@ def home_page():
 @app.route("/test")
 def test():
     return render_template('test.html')
+
