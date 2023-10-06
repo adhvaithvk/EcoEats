7aff3afe2162a6703921f8363531c866993e1dda
RithulBhat <rithul.bhat@gmail.com>
2023-10-06 13:09:04 -0500

    ogay

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def login():
+    return render_template('login.html')
+
+@app.route("/home")
 def home_page():
     return render_template('index.html')
 
